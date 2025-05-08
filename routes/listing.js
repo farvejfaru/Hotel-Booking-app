2619a72351379fde91745119b12d1811091e9d4c
Farvej Farru <farvejfaru128@gmail.com>
2025-05-08 12:54:15 +0530

    Phase-2 Part-B has has completed

---
+++ routes/listing.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { listingSchema } = require("../schema.js");
+const Listing = require("../models/listing.js");
+
+// Validation middleware
+const validateListing = (req, res, next) => {
+    if (!req.body || !req.body.listing) {
+        throw new ExpressError(400, "Listing data is required.");
+    }
+    const { error } = listingSchema.validate(req.body);
+    if (error) {
+        const errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+// Index Route - Show all listings
+router.get("/", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+
+// New Listing Form Route
+router.get("/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+// Show Route - Show one listing
+router.get("/:id", wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+}));
+
+// Create Route - Add new listing
+router.post("/", validateListing, wrapAsync(async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect(`/listings/${newListing._id}`);
+}));
+
+// Edit Form Route
+router.get("/:id/edit", wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}));
+
+// Update Route
+router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+}));
+
+// Delete Route
+router.delete("/:id", wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}));
+
+module.exports = router;
